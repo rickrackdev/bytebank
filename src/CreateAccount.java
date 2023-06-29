bb51643143bc6ce24dae00751db158ee0c1670fe
rick <alvarenga409@gmail.com>
2023-06-28 23:07:00 -0600

    v1.0 of bytebank project, create an object and 2 instances of it with balance

--- src/CreateAccount.java
+++ src/CreateAccount.java
@@ -3,11 +3,16 @@ public class CreateAccount {
     public static void main(String[] args){
       // variable          = value
       Account firstAccount = new Account();
-      firstAccount.balance = 1000;
+      firstAccount.balance = 200;
       System.out.println(firstAccount.balance);
+        firstAccount.balance += 100;
+        System.out.println(firstAccount.balance);
 
       Account secondAccount = new Account();
-      secondAccount.balance = 500;
+      secondAccount.balance = 50;
         System.out.println(secondAccount.balance);
+
+        System.out.println("first account balance: $" + firstAccount.balance);
+        System.out.println("second account balance: $" + secondAccount.balance);
     }
 }
