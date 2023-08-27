322bb46500acef572f687a2231ced071f5e63ff1
rick <alvarenga409@gmail.com>
2023-08-27 01:23:29 -0600

    new commit to continue course lessons

--- src/Account.java
+++ src/Account.java
@@ -6,16 +6,26 @@ class Account {
 
     //deposit does not return a value this is established by the type of method
     public void deposit(double balance) {
-        this.balance = this.balance + balance;
+        this.balance += balance;
     }
 
     //withdraw does return a value
     public boolean withdraw(double balance) {
         if (this.balance >= balance) {
-            this.balance = this.balance - balance;
+            this.balance -= balance;
             return true;
-        } else{
+        } else {
             return false;
             }
         }
+
+    public boolean transfer(double balance, Account account){
+        if (this.balance >= balance){
+            this.balance -= balance;
+            account.deposit(balance);
+            return true;
+        } else {
+            return false;
+        }
+    }
 }
--- src/MethodsTest.java
+++ src/MethodsTest.java
@@ -8,5 +8,15 @@ public class MethodsTest {
 
         myAccount.withdraw(100);
         System.out.println(myAccount.balance);
+
+        Account accountJimena = new Account();
+        accountJimena.deposit(1000);
+        System.out.println(accountJimena.balance + " jimena");
+        boolean canTransfer = accountJimena.transfer(400, myAccount);
+        if (canTransfer){
+            System.out.println("Transfer successful");
+        }
+        System.out.println(myAccount.balance);
+        System.out.println(accountJimena.balance + " jimena");
     }
 }
